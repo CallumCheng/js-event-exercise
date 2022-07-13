191db352ec5e463d7875feaa38e8c86dc1548532
CallumCheng <callumcheng@outlook.com>
2022-07-13 12:58:59 +0100

    js events!

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,25 @@
+    const h1 = document.getElementById('h1');
+    const h2 = document.getElementById('h2');
+    
 
-    document.getElementsById("h1").addEventListener("click", (e) => {
-        e.target.style.backgroundColor = 'salmon';
+    h1.addEventListener("click", function onClick(event) {
+        // 👇️ change background color
+        document.body.style.backgroundColor = 'salmon';
     })
+
+    h1.addEventListener("mouseout", (event) => {
+        document.body.style.backgroundColor = 'white';
+    })
+
+    h2.addEventListener("mouseover", (e) => {
+        e.target.textContent = "Hello there, You have won a million pounds"
+    })
+
+    h2.addEventListener("mouseout", (e) => {
+        e.target.textContent = "This is the H2 Tag"
+    })
+
+
+
+
+
